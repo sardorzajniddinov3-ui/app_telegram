5fde7109e7f8ff43e57d9a336c34010005917656
Sardor <sardor@example.com>
2026-01-31 13:43:11 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './index.css'
 import './App.css'
 
-const ReactDOM = { createRoot }
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
