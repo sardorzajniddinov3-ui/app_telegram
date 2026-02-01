528b2fa76c7dff17be75cdf8d0e2c5b75fd20f7e
Sardor <sardor@example.com>
2026-02-01 23:44:33 +0500

    set railway api

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rjfchznkmulatifulele.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZmNoem5rbXVsYXRpZnVsZWxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2NjcxNjYsImV4cCI6MjA4NTI0MzE2Nn0.zKh_FO2M2htwW8-PrdC6wlsYiK-XB0irOAlAvqro5YE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
